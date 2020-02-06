7c506cc84d698084161ee1a7758ed00fc8ec6d7c
Chandan746 <44798070+Chandan746@users.noreply.github.com>
2020-02-06 14:15:04 +0530

    Update urls.py

--- webline_notifications/urls.py
+++ webline_notifications/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-
+app_name = 'webline_notifications'
 urlpatterns = [
     url(r'^see-all/$',
         views.see_all_notification, name='see_all_notification'),
